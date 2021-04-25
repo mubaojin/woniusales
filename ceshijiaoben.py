0923eb4c6ea5ecbba0e489d9b05d46225a770d2c
mubaojin <2488378734@qq.com>
2021-04-25 13:35:39 +0800

    test

---
+++ ceshijiaoben.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get('http://121.5.205.252:8050/woniusales/')
+
+driver.maximize_window()
+
+driver.find_element('id','username').send_keys('oooaaa11')
+driver.find_element('id','password').send_keys('simonvic11')
+driver.find_element('css selector','.form-control.btn-primary').click()
